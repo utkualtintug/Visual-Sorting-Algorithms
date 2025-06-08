3eacce52de06558f0a0c9c4b9f49095399f3e051
Utku Altintug <utkualtintug@gmail.com>
2025-06-08 21:35:30 +0200

    feat: the issues have been resolved, and a button has been added to add other algorithms

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ canvas_height = 450
 speed = 50
 is_paused = False
 is_sorting = False
+is_reseted = False
 current_i = 0
 current_j = 0
+barHeight = []
+insertion_key = None
 
 
 win = Tk()
@@ -16,10 +19,20 @@ win.geometry("500x650")
 
 canvas = Canvas(win, width=450, height=450, bg="gray")
 canvas.grid(row=0, column=0, padx=25, pady=10)
-barHeight = [random.randint(10,400) for _ in range(50)]
 
+def randomNumber():
+    global barHeight, is_reseted
+    is_reseted = False
+    aList = [random.randint(10,400) for _ in range(50)]
+    barHeight = aList
+    return barHeight
 
 def generate(highlight_indices=None, color_override=None):
+    global barHeight
+    if len(barHeight) == 0:
+        randomNumber() 
+        
+    reset_button["state"] = NORMAL
     canvas.delete("all")
     bar_width = canvas_width / len(barHeight)
     for i in range(len(barHeight)):
@@ -38,6 +51,20 @@ def generate(highlight_indices=None, color_override=None):
         canvas.create_rectangle(x0, y0, x1, y1, fill=color)
         
 
+def sorting():
+    global is_paused, is_sorting
+    switch()
+    if is_sorting:
+        return
+    is_paused = False
+    is_sorting = True
+
+    if selected_algo.get() == "Bubble":
+        bubble_step(0, 0)
+    elif selected_algo.get() == "Insertion":
+        insertion_step(1, 0)
+
+
 def bubble_step(i,j):
     global is_paused, current_i, current_j, is_sorting
     n = len(barHeight)
@@ -51,6 +78,7 @@ def bubble_step(i,j):
     if i >= len(barHeight) - 1:
         generate(color_override="#5cb85c")
         is_sorting = False
+        sort_button["state"] = NORMAL
         return
 
     if j < n - i - 1:
@@ -62,15 +90,28 @@ def bubble_step(i,j):
         canvas.after(speed, lambda: bubble_step(i + 1, 0))
 
 
-def bubble():
-    global is_paused, is_sorting
-    switch()
-    if is_sorting:
+def insertion_step(i, j):
+    global is_paused, current_i, current_j, is_sorting
+    
+    if is_paused:
+        current_i = i
+        current_j = j
+        is_sorting = False
         return
-    is_paused = False
-    is_sorting = True
-    bubble_step(0, 0)
 
+    if i >= len(barHeight) - 1:
+        generate(color_override="#5cb85c")
+        is_sorting = False
+        sort_button["state"] = NORMAL
+        return
+
+#FIXME - #* Fix and rewrite the algorithm's logic. Try to learn.
+
+    for i in range(1, len(barHeight)):
+        j = i
+        while j > 0 and barHeight[j - 1] > barHeight[j]:
+            barHeight[j], barHeight[j - 1] = barHeight[j - 1], barHeight[j]
+            j = j - 1
 
 def faster():
     global speed
@@ -104,14 +145,30 @@ def switch():
         sort_button["state"] = NORMAL
 
 
+def reset():
+    global barHeight, is_reseted,is_paused
+    is_reseted = True
+    is_paused = True
+    reset_button["state"] = DISABLED
+    if is_sorting:
+        sort_button["state"] = NORMAL
+    barHeight.clear()
+    canvas.delete("all")
+
 main_frame = Frame(win)
 main_frame.grid(row=1, column=0, pady=10)
 
 generate_button = Button(main_frame, text="Generate", command=generate)
 generate_button.pack(side=LEFT, padx=5)
 
-sort_button = Button(main_frame, text="Sort", command=bubble)
+sort_button = Button(main_frame, text="Sort", command=sorting)
 sort_button.pack(side=LEFT, padx=5)
+
+selected_algo = StringVar()
+selected_algo.set("Bubble")
+
+algo_menu = OptionMenu(main_frame, selected_algo, "Bubble", "Insertion")
+algo_menu.pack(side=LEFT, padx=5)
 
 button_frame = Frame(win)
 button_frame.grid(row=2, column=0, pady=10)
@@ -130,5 +187,8 @@ stop_button.pack(side=LEFT, padx=5)
 
 continueSort_button = Button(run_frame, text="▶️", command=continueSort)
 continueSort_button.pack(side=LEFT, padx=5)
+
+reset_button = Button(run_frame, text="Reset", command=reset)
+reset_button.pack(side=LEFT, padx=5)
 
 win.mainloop()
